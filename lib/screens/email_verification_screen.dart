53d426b016c385bfd3d11b155903c0001ae9b6c4
lo-oord <3.16630049e+08+lo-oord@users.noreply.github.com>
2026-09-12 19:34:50 +0000

    fix: import auth error mapping

--- lib/screens/email_verification_screen.dart
+++ lib/screens/email_verification_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/app_state_provider.dart';
+import '../services/appwrite_service.dart';
 import '../theme/app_theme.dart';
 import '../utils/toast_utils.dart';
 import '../widgets/auth_branding.dart';
